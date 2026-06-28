6a40711e27b84086ef4676ced61b80560280bc7a
ZachTheMighty <promoted66@gmail.com>
2026-06-28 14:07:03 +0000

    fix: "abc" worked the first time as a coincidence

--- functions/caesar_cipher.js
+++ functions/caesar_cipher.js
@@ -4,10 +4,15 @@ export default function (string, shift) {
   let cypheredText = "";
   const shiftedAlphabet = shiftAlphabet(shift);
 
-  for (let i = 0; i < string.length; i++) cypheredText += shiftedAlphabet[i];
+  for (let i = 0; i < string.length; i++)
+    cypheredText += shiftedAlphabet[getIndexInAlphabet(string[i])];
   return cypheredText;
 }
 
 function shiftAlphabet(shift) {
   return ALPHABET.slice(shift) + ALPHABET.slice(0, shift);
+}
+
+function getIndexInAlphabet(char) {
+  for (let i = 0; i < ALPHABET.length; i++) if (ALPHABET[i] === char) return i;
 }

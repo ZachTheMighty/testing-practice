005e8f06253923bb3de5dd2aef348d137af40879
ZachTheMighty <promoted66@gmail.com>
2026-06-28 13:44:17 +0000

    add: function for shifting the alphabet

--- functions/caesar_cipher.js
+++ functions/caesar_cipher.js
@@ -1,3 +1,8 @@
 export default function (string, shift) {
   return "def";
 }
+
+function shiftAlphabet(shift) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  return alphabet.slice(shift) + alphabet.slice(0, shift);
+}

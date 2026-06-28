68d350a53a4d2daa076e9a7fb936be8405909e4a
ZachTheMighty <promoted66@gmail.com>
2026-06-28 14:32:02 +0000

    add: bare minimum code to pass analyze test (green)

---
+++ functions/analyze_array.js
@@ -0,0 +1,8 @@
+export default function (array) {
+  return {
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  };
+}
--- tests/analyze_array.spec.js
+++ tests/analyze_array.spec.js
@@ -1,3 +1,5 @@
+import analyzeArray from "../functions/analyze_array.js";
+
 test("analyze [1, 8, 3, 4, 2, 6]", () => {
   expect(analyzeArray([1, 8, 3, 4, 2, 6])).toEqual({
     average: 4,

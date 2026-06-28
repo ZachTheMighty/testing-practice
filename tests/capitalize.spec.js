6ab2ef71a4cc467f6b40062fe9151363786700f3
ZachTheMighty <promoted66@gmail.com>
2026-06-28 12:54:43 +0000

    add: another test (red phase)

--- tests/capitalize.spec.js
+++ tests/capitalize.spec.js
@@ -3,3 +3,7 @@ import capitalize from "../functions/capitalize.js";
 test("capitalize all lowercase", () => {
   expect(capitalize("bitchassnigga")).toBe("Bitchassnigga");
 });
+
+test("capitalize all uppercase", () => {
+  expect(capitalize("BITCHASSNIGGA")).toBe("BITCHASSNIGGA");
+});

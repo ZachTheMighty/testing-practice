ecaef70e3470fcd4037fecf86271c5c9549f3814
ZachTheMighty <promoted66@gmail.com>
2026-06-28 13:59:03 +0000

    refactor: caesarCipher() (refactor)

--- functions/caesar_cipher.js
+++ functions/caesar_cipher.js
@@ -1,13 +1,13 @@
 const ALPHABET = "abcdefghijklmnopqrstuvwxyz";
 
 export default function (string, shift) {
-  return "def";
+  let cypheredText = "";
+  const shiftedAlphabet = shiftAlphabet(shift);
+
+  for (let i = 0; i < string.length; i++) cypheredText += shiftedAlphabet[i];
+  return cypheredText;
 }
 
 function shiftAlphabet(shift) {
   return ALPHABET.slice(shift) + ALPHABET.slice(0, shift);
-}
-
-function getMappedChar(charIndex, shift) {
-  return shiftAlphabet(shift).charAt(charIndex);
 }

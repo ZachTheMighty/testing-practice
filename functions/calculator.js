27bb627f31f912fdab4422c8b17d20766ec845c8
ZachTheMighty <promoted66@gmail.com>
2026-06-28 13:11:24 +0000

    add: bare minimum code to pass summing test (green phase)

---
+++ functions/calculator.js
@@ -0,0 +1,5 @@
+export default {
+  add(a, b) {
+    return 12;
+  },
+};
--- tests/calculator.spec.js
+++ tests/calculator.spec.js
@@ -1,3 +1,5 @@
+import calculator from "../functions/calculator.js";
+
 test("5 + 7", () => {
   expect(calculator.add(5, 7)).toBe(12);
 });
